edbb2a873bdd460273d0234e06b861f2f1a4357b
riicardods <riicardoljn@icloud.com>
2024-01-01 01:25:07 -0300

    Validação do Email

--- CadastroCompleto_API/Models/Usuario.cs
+++ CadastroCompleto_API/Models/Usuario.cs
@@ -1,9 +1,13 @@
-﻿namespace CadastroCompleto_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroCompleto_API.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo de e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Por favor, insira um endereço de e-mail válido.")]
         public string Email { get; set; }
     }
 }
