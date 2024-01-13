cca44372773ec13cac1f8628e758b6cf07ea7743
riicardods <riicardoljn@icloud.com>
2024-01-13 06:39:20 -0300

    A

--- CadastroCompleto_API/Data/CadastroContext.cs
+++ CadastroCompleto_API/Data/CadastroContext.cs
@@ -3,6 +3,7 @@ using CadastroCompleto_API.Models;
 
 namespace CadastroCompleto_API.Data
 {
+
     public class CadastroContext : DbContext
     {
         public CadastroContext(DbContextOptions<CadastroContext> options)
